8889d6950dc49c4e9b49a62dc4478945b947ab06
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-04-08 15:42:51 +0100

    sd

--- zotero-dashboard.py
+++ zotero-dashboard.py
@@ -58,60 +58,60 @@ api_key = '' # api_key is only needed for private groups and libraries
 
 display = st.button('Display dashboard')
 
-if  display:
+with st.spinner('Retrieving data & updating dashboard...'):
+    if  display:
+        zot = zotero.Zotero(library_id, library_type)
 
-    zot = zotero.Zotero(library_id, library_type)
+        @st.cache_data(ttl=600)
+        def zotero_data(library_id, library_type):
+            items = zot.everything(zot.top())
 
-    @st.cache_data(ttl=600)
-    def zotero_data(library_id, library_type):
-        items = zot.everything(zot.top())
+            data=[]
+            columns = ['Title','Publication type', 'Abstract', 'Date published', 'Publisher', 'Journal']
 
-        data=[]
-        columns = ['Title','Publication type', 'Abstract', 'Date published', 'Publisher', 'Journal']
+            for item in items:
+                creators = item['data']['creators']
+                creators_str = ", ".join([creator.get('firstName', '') + ' ' + creator.get('lastName', '') for creator in creators])
+                data.append((
+                    item['data']['title'], 
+                    item['data']['itemType'], 
+                    item['data']['abstractNote'], 
+                    item['data'].get('date'),
+                    item['data'].get('publisher'),
+                    item['data'].get('publicationTitle')
+                    )) 
+            df = pd.DataFrame(data, columns=columns)
+            return df
 
-        for item in items:
-            creators = item['data']['creators']
-            creators_str = ", ".join([creator.get('firstName', '') + ' ' + creator.get('lastName', '') for creator in creators])
-            data.append((
-                item['data']['title'], 
-                item['data']['itemType'], 
-                item['data']['abstractNote'], 
-                item['data'].get('date'),
-                item['data'].get('publisher'),
-                item['data'].get('publicationTitle')
-                )) 
-        df = pd.DataFrame(data, columns=columns)
-        return df
+        df = zotero_data(library_id, library_type)
 
-    df = zotero_data(library_id, library_type)
+        df['Abstract'] = df['Abstract'].replace(r'^\s*$', np.nan, regex=True) # To replace '' with NaN. Otherwise the code below do not understand the value is nan.
+        df['Abstract'] = df['Abstract'].fillna('No abstract')
 
-    df['Abstract'] = df['Abstract'].replace(r'^\s*$', np.nan, regex=True) # To replace '' with NaN. Otherwise the code below do not understand the value is nan.
-    df['Abstract'] = df['Abstract'].fillna('No abstract')
+        # Change type name
+        type_map = {
+            'thesis': 'Thesis',
+            'journalArticle': 'Journal article',
+            'book': 'Book',
+            'bookSection': 'Book chapter',
+            'blogPost': 'Blog post',
+            'videoRecording': 'Video',
+            'podcast': 'Podcast',
+            'magazineArticle': 'Magazine article',
+            'webpage': 'Webpage',
+            'newspaperArticle': 'Newspaper article',
+            'report': 'Report',
+            'forumPost': 'Forum post'
+        }
+        df['Publication type'] = df['Publication type'].replace(type_map)
 
-    # Change type name
-    type_map = {
-        'thesis': 'Thesis',
-        'journalArticle': 'Journal article',
-        'book': 'Book',
-        'bookSection': 'Book chapter',
-        'blogPost': 'Blog post',
-        'videoRecording': 'Video',
-        'podcast': 'Podcast',
-        'magazineArticle': 'Magazine article',
-        'webpage': 'Webpage',
-        'newspaperArticle': 'Newspaper article',
-        'report': 'Report',
-        'forumPost': 'Forum post'
-    }
-    df['Publication type'] = df['Publication type'].replace(type_map)
+        df['Date published'] = pd.to_datetime(df['Date published'], errors='coerce')
+        df['Date published'] = pd.to_datetime(df['Date published'],utc=True).dt.tz_convert('Europe/London')
+        df['Date published'] = df['Date published'].dt.strftime('%d-%m-%Y')
+        df['Date published'] = df['Date published'].fillna('No date')
+        # df['Date published'] = df['Date published'].map(lambda x: x.strftime('%d/%m/%Y') if x else 'No date')
 
-    df['Date published'] = pd.to_datetime(df['Date published'], errors='coerce')
-    df['Date published'] = pd.to_datetime(df['Date published'],utc=True).dt.tz_convert('Europe/London')
-    df['Date published'] = df['Date published'].dt.strftime('%d-%m-%Y')
-    df['Date published'] = df['Date published'].fillna('No date')
-    # df['Date published'] = df['Date published'].map(lambda x: x.strftime('%d/%m/%Y') if x else 'No date')
+        df
 
-    df
-
-else:
-    st.error('Write Zotero library ID')
+    else:
+        st.error('Write Zotero library ID')
