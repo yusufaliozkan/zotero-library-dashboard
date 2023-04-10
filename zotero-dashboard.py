1a9201fd4ede2e4cffcab3aeabe3892e4ffa85d8
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-04-10 16:20:47 +0100

    sd

--- zotero-dashboard.py
+++ zotero-dashboard.py
@@ -91,6 +91,9 @@ with st.spinner('Creating dashboard. This may take a while if the library contai
         df['Abstract'] = df['Abstract'].replace(r'^\s*$', np.nan, regex=True) # To replace '' with NaN. Otherwise the code below do not understand the value is nan.
         df['Abstract'] = df['Abstract'].fillna('No abstract')
 
+        lib_name = df.iloc[:,0]
+        st.write(lib_name)
+
 
         # Change type name
         type_map = {
