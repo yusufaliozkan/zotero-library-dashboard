56c80af072a013e701d1559fb2d7f448c6afedf7
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-04-10 16:24:16 +0100

    sd

--- zotero-dashboard.py
+++ zotero-dashboard.py
@@ -67,6 +67,7 @@ with st.spinner('Creating dashboard. This may take a while if the library contai
         @st.cache_data(ttl=600)
         def zotero_data(library_id, library_type):
             items = zot.everything(zot.top())
+            items
 
             data=[]
             columns = ['Library', 'Title','Publication type', 'Abstract', 'Date published', 'Publisher', 'Journal']
