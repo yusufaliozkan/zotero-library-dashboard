92416f1c4c5b1da37bc81f91b70c92616927b4d7
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-04-11 16:12:13 +0100

    sd

--- zotero-dashboard.py
+++ zotero-dashboard.py
@@ -331,8 +331,6 @@ with st.spinner('Creating dashboard. This may take a while if the library contai
         gpe_counts
         gpe_counts = gpe_counts.groupby('GPE').sum().reset_index()
         gpe_counts.sort_values('count', ascending=False, inplace=True)
-        gpe_counts
-        gpe_counts = gpe_counts.reset_index()
         gpe_counts.head(15)
         
     else:
