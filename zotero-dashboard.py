a1e3c86be82578e3433d1bab7bac0c8f834381bc
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-04-11 14:32:30 +0100

    sd

--- zotero-dashboard.py
+++ zotero-dashboard.py
@@ -22,6 +22,8 @@ from gsheetsdb import connect
 import gsheetsdb as gdb
 import datetime as dt
 import time
+import pycountry
+
 
 st.set_page_config(layout = "wide", 
                     page_title='Zotero library dashboard',
