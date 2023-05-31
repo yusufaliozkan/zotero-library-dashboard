a4d86590427580d777e43601235d0cf1477196f5
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-06-01 00:29:50 +0100

    sd

--- zotero-dashboard.py
+++ zotero-dashboard.py
@@ -82,10 +82,13 @@ try:
             st.caption('Write 2514686 as an example.')
             library_type = 'group'
     
-    else:
+    elif choice == 'Personal library':
         library_id = st.text_input('Personal library ID: ')
         api_key = st.text_input('Zotero API key: ')
         library_type = 'user'
+
+    else:
+        pass
 
     # api_key = '' # api_key is only needed for private groups and libraries
 
