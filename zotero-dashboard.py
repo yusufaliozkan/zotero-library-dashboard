da3b2c45fa78d7f21f81ce89aafd39e7fd1f4efc
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-02-16 22:06:42 +0000

    sd

--- zotero-dashboard.py
+++ zotero-dashboard.py
@@ -5,7 +5,7 @@ from IPython.display import HTML
 import streamlit.components.v1 as components
 import numpy as np
 import altair as alt
-from pandas.io.json import json_normalize
+# from pandas.io.json import json_normalize
 from datetime import date, timedelta  
 import datetime
 from streamlit_extras.switch_page_button import switch_page
